52413f72a068c2eedfb4fe9556ea7b83d9ecb18d
Davod <davidkingnt@gmail.com>
2019-05-27 00:39:46 -0400

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,31 +1,70 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # Local Environment variables
-PUID=$(curl "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['uid'])")
-PGID=$(curl "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['gid'])")
-USER=$(curl "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['username'])")
-LOCAL_HOME=$(curl "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['homeDirectory'])")
-BUCKET=$(curl -f "http://metadata.google.internal/computeMetadata/v1/instance/attributes/BUCKET" -H "Metadata-Flavor: Google")
+PUID=<User ID>
+PGID=<Group ID>
+BUCKET=<Bucket>
+CONF_PATH=<config path>
+WATCH_PATH=<watch path>
+BIN_PATH=<watch path>
+SERVICE_ACCOUNT=<service account name>
+PROJECT_ID=<project ID>
+CONTAINER_NAME="transmission"
+CONTAINER_IMAGE="amitie10g/docker-transmission:latest"
+TZ="UTC"
 
+# Don't modify the following
 CONF_PATH=$LOCAL_HOME/config
 WATCH_PATH=$LOCAL_HOME/watch
+BIN_PATH=$LOCAL_HOME/bin
+
+# --privileged is needed for Linux prior to 4.18
+PRIVILEGED="--privileged"
 
 # Create the directories
-mkdir -p $CONF_PATH $WATCH_PATH
-chown -R $PUID:$PGID $LOCAL_HOME
+mkdir -p $CONF_PATH $WATCH_PATH $BIN_PATH
+chown -R $LOCAL_HOME
 
 # Install or update docker-helper.sh (optional)
-curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/Amitie10g/docker-transmission/gcsfuse/scripts/docker-helper.sh > $LOCAL_HOME/docker-helper.sh
-chmod 755 $LOCAL_HOME/docker-helper.sh
+curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/Amitie10g/docker-transmission/gcsfuse/scripts/docker-helper.sh > $BIN_PATH/docker-helper.sh
+chmod 755 $BIN_PATH/docker-helper.sh
 
-# Download the Account service key from the custom metadata
-curl -f "http://metadata.google.internal/computeMetadata/v1/instance/attributes/gcs-key" \
--H "Metadata-Flavor: Google" -o $CONF_PATH/gcs-key.json
+# Save the variables to /etc/environment
+{
+echo "PUID=\"$PUID\""
+echo "PGID=\"$PGID\""
+echo "BUCKET=\"$BUCKET\""
+echo "TZ=\"$TZ\""
+echo "CONF_PATH=\"$CONF_PATH\""
+echo "WATCH_PATH=\"$WATCH_PATH\""
+echo "CONTAINER_NAME=\"$CONTAINER_NAME\""
+echo "CONTAINER_IMAGE=\"$CONTAINER_IMAGE\""
+echo "PATH=\"$PATH:$BIN_PATH/bin\""
+} >> /etc/environment
 
-# Check if the Service account key has been found
+# Remove duplicated entries
+awk '!a[$0]++' /etc/environment > /tmp/environment
+mv /tmp/environment /etc/environment
+
+# When running on Google Cloud, uncomment in order download the key as Custom metadata
+#curl -f \
+#"http://metadata.google.internal/computeMetadata/v1/instance/attributes/gcs-key" \
+#-H "Metadata-Flavor: Google" > $CONF_PATH/gcs-key.json
+
+# Uncomment if you want to download the key via gcloud, if available and already logged in
+#if [ ! -x "$(command -v gcloud)" ]; then
+#	gcloud iam service-accounts keys create $CONF_PATH/gcs-key.json \
+#	--iam-account $SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com
+#fi
+
 if [ -f "$CONF_PATH/gcs-key.json" ]; then
 	chown -R $PUID:$PGID $CONF_PATH/gcs-key.json
+	# Start the container using docker-helper. If you're using docker-compose or the
+	# Container deployment at Google Cloud, you may comment the following
+	docker-helper start
+	# Workarround for Container-optimized OS (comment above and uncomment bellow)
+	#bash $BIN_PATH/docker-helper.sh start
 else
-	ERROR="Please set the custom metadata gcs-key or upload the key to the VM."
+	ERROR="Please upload the Service Account Key to '\$HOME/config/gcs-key.json', then run 'docker-helper start'."
 	echo "$ERROR" >&2
 fi
