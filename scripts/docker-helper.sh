ad1b3fa73e3664abb4b213e6b2a11c865a0ae261
Davod <davidkingnt@gmail.com>
2019-05-27 20:28:32 -0400

    Update docker-helper.sh

--- scripts/docker-helper.sh
+++ scripts/docker-helper.sh
@@ -1,107 +1,120 @@
 #!/bin/bash
 
 # Set default values for variables
-if [ ! -z "$CONF_PATH" ]
+if [ -z $CONF_PATH ]
 then
-	CONF_PATH="$HOME/config"
+    CONF_PATH="$HOME/config"
 fi
 
-if [ ! -z "$WATCH_PATH" ]
+if [ -z $WATCH_PATH ]
 then
-	WATCH_PATH="$HOME/watch"
+    WATCH_PATH="$HOME/watch"
 fi
 
-if [ ! -z "$CONTAINER_NAME" ]
+if [ -z $CONTAINER_NAME ]
 then
 	CONTAINER_NAME="transmission"
+    #CONTAINER_NAME=$(docker -lq)
 fi
 
-if [ ! -z "$CONTAINER_IMAGE" ]
+if [ -z $CONTAINER_IMAGE ]
 then
-	CONTAINER_IMAGE="amitie10g/docker-transmission:latest"
+    CONTAINER_IMAGE="amitie10g/docker-transmission:latest"
 fi
 
-if [ ! -z "$TZ" ]
+if [ -z $TZ ]
 then
       TZ="UTC"
 fi
 
-if [ ! -z "$CONTAINER_SHELL" ]
+if [ -z $CONTAINER_SHELL ]
 then
-	CONTAINER_SHELL="/bin/bash"
+    CONTAINER_SHELL="/bin/bash"
+fi
+
+# If $PRIVILEGED is set to false, change to null
+if [ "$PRIVILEGED" != false ]
+then
+    PRIVILEGED="--privileged"
+else
+	PRIVILEGED=
 fi
 
 case $1 in
-	build)
-		if [ ! -d "docker-transmission" ]; then
-			git clone --branch gcsfuse https://github.com/Amitie10g/docker-transmission.git
-		fi
-		cd docker-transmission
-		docker build \
-		--no-cache \
-		--pull \
-		--compress \
-		-t $CONTAINER_IMAGE .
-	;;
+    build)
+        if [ ! -d "docker-transmission" ]; then
+            git clone --branch gcsfuse https://github.com/Amitie10g/docker-transmission.git
+        fi
+        cd docker-transmission
+        docker build \
+        --no-cache \
+        --pull \
+        --compress \
+        -t $CONTAINER_IMAGE .
+    ;;
 
-	pull)
-		docker image pull $CONTAINER_IMAGE
+    pull)
+        echo "Retriveing $CONTAINER_IMAGE..."
+        docker image pull $CONTAINER_IMAGE
     ;;
   
-	stop)
-		docker stop $CONTAINER_NAME
-		docker rm $CONTAINER_NAME
-	;;
-    
-	rm|delete)
-		docker stop $CONTAINER_NAME
-		docker rm $CONTAINER_NAME
-		docker image rm $CONTAINER_IMAGE
+    stop)
+        echo "Removing $CONTAINER_IMAGE..."
+        docker stop $CONTAINER_NAME
+        docker rm $CONTAINER_NAME
     ;;
     
-	shell)
-		docker exec -i -t $CONTAINER_NAME $CONTAINER_SHELL
+    rm|delete)
+        echo "Stoping $CONTAINER_NAME and removing $CONTAINER_IMAGE..."
+        docker stop $CONTAINER_NAME
+        docker rm $CONTAINER_NAME
+        docker image rm $CONTAINER_IMAGE
     ;;
     
-	log|logs)
-		docker logs --details $CONTAINER_NAME
+    shell)
+        echo "Opening shell for $CONTAINER_NAME..."
+        docker exec -i -t $CONTAINER_NAME $CONTAINER_SHELL
+    ;;
+    
+    log|logs)
+        echo "Showing logs for $CONTAINER_NAME..."
+        docker logs --details $CONTAINER_NAME
     ;;
 
-	start)
-		docker run -t -i -d \
-			--name=$CONTAINER_NAME \
-			-e PUID=$PUID \
-			-e PGID=$PGID \
-			-e TZ=$TZ \
-			-e BUCKET=$BUCKET \
-			-e TRANSMISSION_WEB_HOME=/combustion-release/ \
-			-p 9091:9091 \
-			-p 51413:51413 \
-			-p 51413:51413/udp \
-			-v $CONF_PATH:/config \
-			-v $WATCH_PATH:/watch \
-			--device=/dev/fuse \
-			--restart unless-stopped \
-			--ipv6 \
-			$PRIVILEGED \
-			$CONTAINER_IMAGE
-	;;
-	*)
-	echo -e "\n\033[1mUsage:\033[0m
+    start)
+        docker run -t -i -d \
+            --name=$CONTAINER_NAME \
+            -e PUID=$PUID \
+            -e PGID=$PGID \
+            -e TZ=$TZ \
+            -e BUCKET=$BUCKET \
+            -e TRANSMISSION_WEB_HOME=/combustion-release/ \
+            -p 9091:9091 \
+            -p 51413:51413 \
+            -p 51413:51413/udp \
+            -v $CONF_PATH:/config \
+            -v $WATCH_PATH:/watch \
+            --device=/dev/fuse \
+            --restart unless-stopped \
+            $PRIVILEGED \
+            $CONTAINER_IMAGE
+    ;;
+
+    *)
+    echo -e "\033[1mUsage:\033[0m docker-helper command
+\033[1mAlternate container:\033[0m env CONTAINER_NAME=<container name> docker-helper command
 \033[1mCommands:\033[0m
-  \033[1mstart\033[0m	starts the container (pull the image from remote registry)
-  \033[1mstop\033[0m	stops the container
-  \033[1mrm\033[0m	stops the container and remove the associated image
-  \033[1mshell\033[0m	gives access to the container shell (/bin/bash by default)
-  \033[1mpull\033[0m	pulls the image to the remote registry
-  \033[1mbuild\033[0m	pulls the source code from repo
+  \033[1mstart\033[0m    starts the container (pull the image from remote registry)
+  \033[1mstop\033[0m    stops the container
+  \033[1mrm\033[0m    stops the container and remove the associated image
+  \033[1mshell\033[0m    gives access to the container shell (/bin/bash by default)
+  \033[1mpull\033[0m    pulls the image to the remote registry
+  \033[1mbuild\033[0m    pulls the source code from repo
   
-\033[1mParameters:\033[0m
-Parameters can be provided via command line or envirnment variables.
-  \033[1mParam	Envirnment variable	Description			Default value\033[0m
-  \033[1m-c\033[0m	\$CONF_PATH		Configuration directory		\$HOME/config
-  \033[1m-w\033[0m	\$WATCH_PATH		The Configuration directory	\$HOME/watch
-  \033[1m-n\033[0m	\$CONTAINER_NAME		The Container name		transmission
-  \033[1m-i\033[0m	\$CONTAINER_IMAGE	The Container image		amitie10g/docker-transmission\n"
+\033[1mEnvirnment variables:\033[0m
+  \033[1m\$CONF_PATH\033[0m        Configuration directory        \$HOME/config
+  \033[1m\$WATCH_PATH\033[0m        The Configuration directory    \$HOME/watch
+  \033[1m\$CONTAINER_NAME\033[0m    The Container name        transmission
+  \033[1m\$CONTAINER_IMAGE\033[0m    The Container image        amitie10g/docker-transmission"
   ;;
 esac
