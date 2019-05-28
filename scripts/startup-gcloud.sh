9c9c3b122c9cbf54d19a1aaa1261fee1dd8e4e97
Davod <davidkingnt@gmail.com>
2019-05-27 20:27:03 -0400

    Create startup-gcloud.sh

---
+++ scripts/startup-gcloud.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Local Environment variables
+PUID=$(curl "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['uid'])")
+PGID=$(curl "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['gid'])")
+USER=$(curl "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['username'])")
+LOCAL_HOME=$(curl "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['homeDirectory'])")
+
+CONF_PATH=$LOCAL_HOME/config
+WATCH_PATH=$LOCAL_HOME/watch
+
+# Create the directories
+mkdir -p $CONF_PATH $WATCH_PATH
+chown -R $PUID:$PGID $LOCAL_HOME
+
+# Install or update docker-helper.sh (optional)
+curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/Amitie10g/docker-transmission/gcsfuse/scripts/docker-helper.sh > $LOCAL_HOME/docker-helper.sh
+chmod 755 $LOCAL_HOME/docker-helper.sh
+
+# Download the Account service key from the custom metadata
+curl -f "http://metadata.google.internal/computeMetadata/v1/instance/attributes/gcs-key" \
+-H "Metadata-Flavor: Google" -o $CONF_PATH/gcs-key.json
+
+# Check if the Service account key has been found
+if [ -f "$CONF_PATH/gcs-key.json" ]; then
+	chown -R $PUID:$PGID $CONF_PATH/gcs-key.json
+else
+	ERROR="Please set the custom metadata gcs-key or upload the key to the VM."
+	echo "$ERROR" >&2
+fi
