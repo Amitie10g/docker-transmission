76ce58b0ab447971a6261144ea726db77ded2849
Davod <davidkingnt@gmail.com>
2019-05-30 18:20:01 -0400

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,9 +5,6 @@ PUID=
 PGID= 
 LOCAL_HOME= 
 BUCKET= 
-CONF_PATH= 
-WATCH_PATH= 
-BIN_PATH= 
 SERVICE_ACCOUNT=
 PROJECT_ID=
 CONTAINER_NAME="transmission"
@@ -17,12 +14,14 @@ TZ="UTC"
 # Don't modify the following
 CONF_PATH=$LOCAL_HOME/config
 WATCH_PATH=$LOCAL_HOME/watch
+COMPLETE_PATH=$LOCAL_HOME/downloads/complete
+INCOMPLETE_PATH=$LOCAL_HOME/downloads/incomplete
 
 # Uncomment if you have Linux 4.18 or above, as --privileged is not longer needed
 #PRIVILEGED=false
 
 # Create the directories
-mkdir -p $CONF_PATH $WATCH_PATH $BIN_PATH
+mkdir -p $CONF_PATH $WATCH_PATH $COMPLETE_PATH $INCOMPLETE_PATH
 chown -R $LOCAL_HOME
 
 # Install or update docker-helper.sh (optional)
@@ -39,6 +38,8 @@ echo "BUCKET=\"$BUCKET\""
 echo "TZ=\"$TZ\""
 echo "CONF_PATH=\"$CONF_PATH\""
 echo "WATCH_PATH=\"$WATCH_PATH\""
+echo "COMPLETE_PATH=\"$COMPLETE_PATH\""
+echo "INCOMPLETE_PATH=\"$INCOMPLETE_PATH\""
 echo "CONTAINER_NAME=\"$CONTAINER_NAME\""
 echo "CONTAINER_IMAGE=\"$CONTAINER_IMAGE\""
 echo "PATH=\"$PATH:$BIN_PATH/bin\""
@@ -58,9 +59,7 @@ if [ -f "$CONF_PATH/gcs-key.json" ]; then
 	chown -R $PUID:$PGID $CONF_PATH/gcs-key.json
 	# Start the container using docker-helper. If you're using docker-compose or the
 	# Container deployment at Google Cloud, you may comment the following
-	$HOME/docker-helper start
-	# Workarround for Container-optimized OS (comment above and uncomment bellow)
-	#bash $HOME/docker-helper.sh start
+	bash $HOME/docker-helper start
 else
 	ERROR="Please upload the Service Account Key to '\$HOME/config/gcs-key.json', then run 'docker-helper start'."
 	echo "$ERROR" >&2
