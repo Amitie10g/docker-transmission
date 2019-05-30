e9887f99ea63532d4382208d8e47aec0e2cb2503
Davod <davidkingnt@gmail.com>
2019-05-29 20:41:29 -0400

    Update startup-gcloud.sh

--- scripts/startup-gcloud.sh
+++ scripts/startup-gcloud.sh
@@ -14,7 +14,7 @@ mkdir -p $CONF_PATH $WATCH_PATH
 chown -R $PUID:$PGID $LOCAL_HOME
 
 # Install or update docker-helper.sh (optional)
-curl -f "https://raw.githubusercontent.com/Amitie10g/docker-transmission/gcsfuse/scripts/docker-helper.sh > $LOCAL_HOME/docker-helper.sh" \
+curl -f "https://github.com/Amitie10g/docker-transmission/raw/gdrive-ocamlfuse/scripts/docker-helper.sh \
 -H 'Cache-Control: no-cache' -o $LOCAL_HOME/docker-helper.sh
 chmod 755 $LOCAL_HOME/docker-helper.sh
 
