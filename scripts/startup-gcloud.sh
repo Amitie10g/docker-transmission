03bce112d4ba29d087411baacb11be22c429e08b
Davod <davidkingnt@gmail.com>
2019-05-29 21:01:03 -0400

    Update startup-gcloud.sh

--- scripts/startup-gcloud.sh
+++ scripts/startup-gcloud.sh
@@ -3,19 +3,19 @@
 # Local Environment variables
 PUID=$(curl -fSs "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['uid'])")
 PGID=$(curl -fSs "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['gid'])")
-HOME=$(curl -fSs "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['homeDirectory'])")
+LOCAL_HOME=$(curl -fSs "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['homeDirectory'])")
 
-CONF_PATH=$HOME/config
-WATCH_PATH=$HOME/watch
+CONF_PATH=$LOCAL_HOME/config
+WATCH_PATH=$LOCAL_HOME/watch
 
 # Create the directories
 mkdir -p $CONF_PATH $WATCH_PATH
-chown -R $PUID:$PGID $HOME
+chown -R $PUID:$PGID $LOCAL_HOME
 
 # Install or update docker-helper.sh (optional)
 curl -fSs "https://raw.githubusercontent.com/Amitie10g/docker-transmission/gcsfuse/scripts/docker-helper.sh \
--H 'Cache-Control: no-cache' -o $HOME/docker-helper.sh
-chmod 755 $HOME/docker-helper.sh
+-H 'Cache-Control: no-cache' -o $LOCAL_HOME/docker-helper.sh
+chmod 755 $LOCAL_HOME/docker-helper.sh
 
 # Download the Settings from custom metadata
 curl -fSs "http://metadata.google.internal/computeMetadata/v1/instance/attributes/SETTINGS" \
