efaa464847353b6ddde4be346ff404d55cc182f0
Davod <davidkingnt@gmail.com>
2019-05-31 20:10:48 -0400

    Update docker-helper.sh

--- scripts/docker-helper.sh
+++ scripts/docker-helper.sh
@@ -69,7 +69,6 @@ case $1 in
 	    -e CLIENT_SECRET="$CLIENT_SECRET" \
 	    -e VERIFICATION_CODE="$VERIFICATION_CODE" \
 	    -e MOUNT_OPTS="$MOUNT_OPTS" \
-	    -e REMOTE_DIR="$REMOTE_DIR" \
 	    -e TEAMDRIVE="$TEAMDRIVE" \
             -e TRANSMISSION_WEB_HOME=/combustion-release/ \
             -v "$CONF_PATH":/config \
